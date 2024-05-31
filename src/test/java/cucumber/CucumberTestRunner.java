46a86dc894b9823ae09360f9c6bc0f37f34d2e77
sri <mettu.sri1@gmail.com>
2024-05-31 15:41:44 +0100

    updates31052024-1

--- src/test/java/cucumber/CucumberTestRunner.java
+++ src/test/java/cucumber/CucumberTestRunner.java
@@ -3,8 +3,12 @@ package cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="src/test/java/cucumber",glue="srimettu.stepDefinitions",
-monochrome=true,plugin= {"html:target/cucumber.html"})
+@CucumberOptions(
+features="src/test/java/cucumber",
+glue="srimettu.stepDefinitions",
+monochrome=true,
+plugin= {"html:target/cucumber.html"})
+
 public class CucumberTestRunner extends AbstractTestNGCucumberTests {
 
 }
